d3f6501f346f2205be1db188ff32977c21f5c924
Matobas <magarciao@ucvvirtual.edu.pe>
2026-08-10 21:26:42 -0500

    cambios para produccion en vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,19 @@
 <?php
 
-// Redirigir las peticiones del index al archivo público de Laravel
-require __DIR__ . '/../public/index.php';
+// 1. Mapear explícitamente los archivos esenciales desde la raíz del proyecto
+require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap/app.php';
+
+// 2. Capturar y manejar la petición HTTP a través de Laravel
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
