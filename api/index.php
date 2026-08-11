42749df4b8a3a6fb6d9a769935eb07e065e83f78
Matobas <magarciao@ucvvirtual.edu.pe>
2026-08-10 22:28:13 -0500

    cambios para produccion en vercel

--- api/index.php
+++ api/index.php
@@ -1,19 +1,19 @@
 <?php
 
-// 1. Mapear explícitamente los archivos esenciales desde la raíz del proyecto
-require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../bootstrap/app.php';
-
-// 2. Capturar y manejar la petición HTTP a través de Laravel
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
+// 1. Cargar el Autoloader de Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Comprobar si la aplicación está en modo mantenimiento
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+// 3. Inicializar la aplicación (¡SOLO UNA VEZ!)
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// 4. Capturar y manejar la petición HTTP a través de Laravel
+use Illuminate\Http\Request;
 
 $app->handleRequest(Request::capture());
